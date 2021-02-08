2b4932cd82b3cf7b21397cef56aa542dbf58faad
Kwanghoon Choi <lazyswamp@gmail.com>
2021-02-08 18:10:25 +0000

    Implemented an html-to-view conversion; Revised the Action, Execute Value, into Execute Value [Value] to support onInput event to take a key input.

--- webclient/app/WebRuntime.hs
+++ webclient/app/WebRuntime.hs
@@ -7,7 +7,10 @@ import Literal
 import Runtime
 
 import Data.Aeson (eitherDecodeStrict, toJSON)
+import Data.Bool
 import Data.List
+import Data.Maybe (fromJust)
+import qualified Data.Map as M (singleton)
 import Control.Monad.Trans.State.Lazy (runStateT)
 
 import           JavaScript.Web.XMLHttpRequest
@@ -43,6 +46,8 @@ import           JavaScript.Web.XMLHttpRequest
 --   | KeyBind Int a
 --   | ValueBind String (String -client-> a)
 
+-- data List = [a]. Nil | Cons a (List [a])
+
 -------------------------------------------------------------------------
 
 mainName = "main"
@@ -53,39 +58,50 @@ instance Eq Model where
   (Model a (b, _, _) c) == (Model d (e, _, _) f) = a == d && b == e && c == f
 
 data Action =
-    Execute Value
+    Execute Value [Value]  -- V [] for Action or V [W] for String -> Action
   | SetResult ((Value, Value), RuntimeState)   -- (a new program model, a new view), prog state
   | NoOp
 
 pageWebApp :: RuntimeFunctionMap -> Value -> RuntimeState
     -> (Model, Model -> View Action, Action -> Model -> Effect Action Model, MisoString)
-pageWebApp runtimeFunMap
-  (Constr "Page" [ initModelValue, viewFunValue, updateFunValue, mount_pointValue ])
+pageWebApp r_FunMap
+  (Constr "Page" [ initModelV, viewFunV, updateFunV, mount_pointV ])
   state =  ( initModel, view, update, miso_mount_point )
     
   where
-    initModel = Model { progModel=initModelValue, progState=state, updatedView = Nothing }
+    ----------------------------------------------------------------------------
+    -- | Initial model
+    ----------------------------------------------------------------------------
+    initModel = Model { progModel=initModelV
+                      , progState=state
+                      , updatedView = Nothing }
 
+    ----------------------------------------------------------------------------
+    -- | view
+    ----------------------------------------------------------------------------
     view :: Model -> View Action
-    view model = do
-      error $ "Not supported yet"  -- Todo: Fix it!
+    view model = htmlToView $ fromJust $ updatedView model
       
-      -- htmlValue <- apply runtimeFunMap viewFunValue model 
-      -- return fromHtmlToView htmlValue
-
     ----------------------------------------------------------------------------
     -- | update
     ----------------------------------------------------------------------------
     update :: Action -> Model -> Effect Action Model
     
-    update (Execute argActionValue) model = model <# do
-      SetResult <$> doExecute runtimeFunMap model updateFunValue argActionValue viewFunValue
+    update (Execute argActionValue parmsV) model = model <# do
+      SetResult <$> doExecute r_FunMap model updateFunV argActionValue parmsV viewFunV
       
     update (SetResult ((_progModel, _updatedView), _progState)) model =
-      noEff Model { progModel=_progModel, progState=_progState, updatedView=Just _updatedView }
-    ----------------------------------------------------------------------------
+      noEff Model { progModel=_progModel
+                  , progState=_progState
+                  , updatedView=Just _updatedView }
 
-    miso_mount_point = toMiso_String mount_pointValue
+    update NoOp model = noEff model
+
+    ----------------------------------------------------------------------------
+    -- | mount point
+    ----------------------------------------------------------------------------
+    miso_mount_point = toMiso_String mount_pointV
+    ----------------------------------------------------------------------------
 
 pageWebApp runtimeFunMap pageValue state =
   error $ "[pageWebApp] Unexpected page structure: " ++ show pageValue
@@ -93,13 +109,20 @@ pageWebApp runtimeFunMap pageValue state =
 
 -- | For update
 
-doExecute :: RuntimeFunctionMap -> Model -> Value -> Value -> Value -> IO ((Value, Value), RuntimeState)
-doExecute runtimeFunMap model updateValue argActionValue viewValue = do
-  runStateT (do 
-     updateAction <- apply runtimeFunMap updateValue argActionValue
+doExecute :: RuntimeFunctionMap -> Model -> Value -> Value -> [Value] -> Value -> IO ((Value, Value), RuntimeState)
+doExecute runtimeFunMap model updateV funActionV parmsV viewV = do
+  runStateT (do
+     argActionV <-
+      case parmsV of
+       [] -> return funActionV
+       [parmV] -> apply runtimeFunMap funActionV parmV
+       _ -> error $ "[WebRuntime:webModel:doExecute] Not support more than one parm: "
+                        ++ show (Data.List.length parmsV)
+        
+     updateAction <- apply runtimeFunMap updateV argActionV
      newModel <- apply runtimeFunMap updateAction (progModel model)
      
-     newView <- apply runtimeFunMap viewValue newModel
+     newView <- apply runtimeFunMap viewV newModel
      
      return (newModel, newView) ) (progState model)
 
@@ -135,6 +158,88 @@ toMiso_String v = error $ "[WebRuntime: toMiso_String] Expected string literal b
 
 -- | For view
 
-fromHtmlToView :: Value -> View Action  -- Value=HTML [Msg], View Value=View Action
-fromHtmlToView htmlValue = error $ "[WebRuntime:fromHtmlToView] Not supported yet" -- Todo: Fix it!
+htmlToView :: Value -> View Action  -- Value=HTML [Msg], View Action
 
+htmlToView (Constr "Element" [Lit (StrLit tag), attrs, htmls]) = htmlToView' tag
+  where
+    attrAttrs = attrToViews attrs
+    htmlViews = htmlToViews htmls  
+
+    htmlToView' "h1" = h1_ attrAttrs htmlViews
+    htmlToView' "h2" = h2_ attrAttrs htmlViews
+    htmlToView' "h3" = h3_ attrAttrs htmlViews
+    htmlToView' "h4" = h4_ attrAttrs htmlViews
+    htmlToView' "h5" = h5_ attrAttrs htmlViews
+    htmlToView' "h6" = h6_ attrAttrs htmlViews
+    htmlToView' tag = error $ "[WebRuntime:htmlToView] Not supported yet: " ++ tag
+  
+htmlToView (Constr "Txt" [Lit (StrLit textLit)]) = text $ pack textLit
+
+htmlToView v = error $ "[WebRuntime:htmlToView] Unexpected: " ++ show v
+
+--
+htmlToViews :: Value -> [View Action]
+
+htmlToViews (Constr "Nil" []) = []
+
+htmlToViews (Constr "Cons" [h, t]) =
+  htmlToView h : htmlToViews t
+
+htmlToViews v = error $ "[WebRuntime:htmlToViews] Unexpected: " ++ show v
+
+--
+attrToView :: Value -> Attribute Action -- Value=Attr [Msg], View Action
+
+attrToView (Constr "Property" [Lit (StrLit keyText), Lit (StrLit valueText)]) = 
+  stringProp (pack keyText) valueText
+
+attrToView (Constr "Attribute" [Lit (StrLit keyText), Lit (StrLit valueText)]) =
+  attrToView' keyText
+  where
+    attrToView' "class" = class_ $ pack valueText
+    attrToView' "type" = type_ $ pack valueText
+    attrToView' "style" = style_ $ toStyleMap valueText
+    attrToView' "id" = id_ $ pack valueText
+    attrToView' "for" = for_ $ pack valueText
+    attrToView' "placeholder" = placeholder_ $ pack valueText
+    attrToView' "value" = value_ $ pack valueText
+    attrToView' _ = error $ "[WebRuntime:attrToViews] Attribute: Unexpected: "
+                              ++ show keyText ++ "=" ++ valueText
+
+    toStyleMap "display; block" = M.singleton (pack "display") (pack "block")
+
+attrToView (Constr "EventBind" [Lit (StrLit eventName), msgV]) = attrToView' eventName
+  where
+    packedEventName = pack eventName
+    
+    attrToView' "click"    = onClick $ Execute msgV []
+    attrToView' "dblclick" = onDoubleClick $ Execute msgV []
+    attrToView' "blur"     = onBlur $ Execute msgV []
+    attrToView' _ = error $ "[WebRuntime:attrToView] EventBind: Not supported yet:" ++ eventName
+
+attrToView (Constr "KeyBind" [Lit (IntLit num), msgV]) = attrToView' num
+  where
+    attrToView' 13 = onEnter $ Execute msgV []
+    attrToView' _ = error $ "[WebRuntime:attrToView] KeyBind: Not supported yet:" ++ show num
+
+    onEnter :: Action -> Attribute Action
+    onEnter action = onKeyDown $ bool NoOp action . (== KeyCode 13)
+  
+
+attrToView (Constr "ValueBind" [Lit (StrLit keyText), strToMsgFunV]) = attrToView' keyText
+  where
+    attrToView' "input" = onInput $ \x -> Execute strToMsgFunV [Lit (StrLit $ unpack $ x)]
+    attrToView' _ = error $ "[WebRuntime:attrToView] ValueBind: Not supported yet:" ++ keyText
+
+attrToView v = 
+  error $ "[WebRuntime:attrToView] Unexpected: " ++ show v
+
+--
+attrToViews :: Value -> [Attribute Action]
+
+attrToViews (Constr "Nil" []) = []
+
+attrToViews (Constr "Cons" [h, t]) =
+  attrToView h : attrToViews t
+
+attrToViews v = error $ "[WebRuntime:attrToViews] Unexpected: " ++ show v
